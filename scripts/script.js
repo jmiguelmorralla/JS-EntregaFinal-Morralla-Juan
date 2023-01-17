c10c9834b38ffc3c5ab738ea1c25b9d72e1d73f7
jmiguelmorralla <jmiguelmorralla@gmail.com>
2023-01-17 01:47:29 -0300

    Incorporación de imágenes y dinamica de elementos.

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 //Variables
 
 const formulario = document.querySelector("#formulario");
+const datosCliente = document.querySelector("#datosCliente");
 const nombre = document.querySelector("#nombre");
 const apellido = document.querySelector("#apellido");
 const mail = document.querySelector("#mail");
@@ -10,9 +11,11 @@ const espacioCliente = document.querySelector("#espacioCliente");
 const contenedorProductos = document.querySelector(".contenedor-productos");
 const espacioCarrito = document.querySelector("#espacioCarrito");
 const totalCarrito = document.querySelector("#totalCarrito");
+const divCarrito = document.querySelector("#divCarrito")
 const botonVaciarCarrito = document.querySelector("#botonVaciarCarrito");
 const precioTotal = document.querySelector("#precioTotal");
-let cart = document.querySelector("#cart");
+const botonesFinal = document.querySelector("#botonesFinal");
+let botonCarrito = document.querySelector("#cart");
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 document.addEventListener("DOMContentLoaded", bootstrap);
@@ -27,11 +30,15 @@ function bootstrap() {
   mostrarCarrito();
 }
 
+// EVENTOS DEL CARRITO
+
 function agregarEventosCarrito() {
-  cart.addEventListener("click", irACarrito);
+  botonCarrito.addEventListener("click", irACarrito);
   botonVaciarCarrito.addEventListener("click", limpiarCarrito);
   botonConfirmarCompra.addEventListener("click", confirmarCompra);
 }
+
+// EVENTOS DEL FORMULARIO
 
 function agregarEventosFormulario() {
   formulario.addEventListener("submit", registrar);
@@ -75,22 +82,26 @@ function agregarEventosFormulario() {
       "Apellido"
     )} <br> Correo: ${localStorage.getItem(
       "Correo"
-    )}<br> <br> <button type="submit" class="btn btn-outline-info" id="botonOcultar">Ocultar datos</button>`;
+    )}<br> <br> <button type="submit" class="btn btn-outline-info" id="botonOcultar">Ocultar datos</button> <br><br>`;
 
     const botonOcultar = document.querySelector("#botonOcultar");
 
     botonOcultar.addEventListener("click", function (e) {
       e.preventDefault(e);
-      espacioCliente.innerHTML = `Se ocultaron los datos de cliente.`;
+      espacioCliente.innerHTML = `Se ocultaron los datos de cliente.<br><br>`;
     });
   });
 }
+
+// CARGAR LOS PRODUCTOS DEL .JSON
 
 const cargarProductos = async () => {
   const respuesta = await fetch("../scripts/datos.json");
   const datos = await respuesta.json();
   return datos;
 }
+
+// MOSTRAR LOS PRODUCTOS
 
 function mostrarProductos(datos) {
   datos.forEach((productosMostrados) => {
@@ -102,8 +113,9 @@ function mostrarProductos(datos) {
                 <div class="card-body">
                     <h6 id="id">${productosMostrados.id}</h6>
                     <h6 id="cantidad">${productosMostrados.cantidad}</h6>
+                    <img class="fluid imagenCatalogo" src="${productosMostrados.img}">
                     <h5 class="card-title">${productosMostrados.nombre}</h5>
-                    <p class="card-text">${productosMostrados.precio}</p>
+                    <p class="card-text">$${productosMostrados.precio}</p>
                     <button class="btn btn-agregar btn-warning" id="${productosMostrados.id}">Agregar</button>
                 </div>
             </div>`;
@@ -118,21 +130,36 @@ function mostrarProductos(datos) {
   botonAgregarProducto.forEach(btn => btn.addEventListener("click", (e) => {
   agregarProductoCarrito(e.target.id, datos);
   irACarrito();
+  Toastify({
+    text: "Agregaste un hermoso producto a tu carrito.",
+    duration: 3000,
+    destination: "../pages/pagos.html",
+    newWindow: true,
+    close: true,
+    gravity: "bottom",
+    position: "right",
+    stopOnFocus: true,
+    style: {
+      background: "linear-gradient(to right, #fc8803, #fc3d03)",
+    },
+    onClick: function () {},
+  }).showToast();
   }))
 }
+
+// IR AL CARRITO
 
 function irACarrito() {
   location.hash = "espacioCarrito";
 }
 
+// AGREGAR LOS PRODUCTOS AL CARRITO
+
 function agregarProductoCarrito(id, producto) {
 
   const productoAgregar = producto.find(p => p.id === id);
-  console.log(carrito)
-  
+ 
   const existe = carrito.some(item => item.id === id);
-  
-  console.log(existe)
   
   if (existe) {
     const prod = carrito.map (prod => {
@@ -148,11 +175,15 @@ function agregarProductoCarrito(id, producto) {
   mostrarCarrito();
 }
 
+// ELIMINAR CADA PRODUCTO DEL CARRITO
+
 function eliminarProducto(producto) {
   const item = carrito.find(temp => temp.id === producto.id);
   carrito = carrito.filter(temp => temp.id !== producto.id);
   mostrarCarrito();
 }
+
+// MOSTRAR CARRITO - ACTUALIZAR EL CARRITO
 
 function mostrarCarrito() {
   espacioCarrito.innerHTML = "";
@@ -207,7 +238,13 @@ function mostrarCarrito() {
   /* NUMERO EN BOTÓN CARRITO */
 
   let cant = document.querySelector("#cant");
-  cant.innerHTML = carrito.length;
+
+  cant.textContent = carrito.reduce(
+    (acc, prod) => acc + prod.cantidad,
+    0
+  );
+
+  cant.innerHTML = cant.textContent;
 
   /* TOTALIZADOR */
   precioTotal.textContent = carrito.reduce(
@@ -216,7 +253,20 @@ function mostrarCarrito() {
   );
 
   localStorage.setItem("carrito", JSON.stringify(carrito));
+
+  if (carrito.length === 0) {
+    datosCliente.style.display="block";
+    formulario.style.display="block";
+    divCarrito.style.display="none";
+
+  } else {
+    datosCliente.style.display="none";
+    formulario.style.display="none";
+    divCarrito.style.display="block";
+  }
 }
+
+// VACIAR CARRITO 
 
 function limpiarCarrito() {
   swal({
@@ -239,6 +289,8 @@ function limpiarCarrito() {
   });
 }
 
+// CONFIRMAR COMPRA
+
 function confirmarCompra() {
   swal({
     title: "¿Estás segur@?",
@@ -252,26 +304,15 @@ function confirmarCompra() {
         icon: "success",
         buttons: false,
       });
-      Toastify({
-        text: "Ya casi terminás tu compra. Solo te falta confirmar la forma de envío y pago.",
-        duration: 10000,
-        destination: "../pages/pagos.html",
-        newWindow: true,
-        close: true,
-        gravity: "bottom",
-        position: "right",
-        stopOnFocus: true,
-        style: {
-          background: "linear-gradient(to right, #fc8803, #fc3d03)",
-        },
-        onClick: function () {},
-      }).showToast();
+      
 
       setTimeout(() => {
         location.href = "../pages/pagos.html";
-      }, 5000);
+      }, 3500);
     } else {
       swal("Podés seguir agregando productos a tu carrito.");
     }
   });
 }
+
+
